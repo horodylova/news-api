a315402689e036aa267fb57b3dcea92bbe8801a5
Svitlana Horodylova <horodylova.sv@gmail.com>
2024-07-15 14:59:15 +0100

    added topics endpoint with tests
    tests for: 404 not found, 500 server error, 200 with body.length, typeof responses
    
    added model and controller for api/topics get request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -24,3 +24,57 @@ describe('GET /api', () => {
         });
     });
   });
+
+  describe("invalid endpoint", () => {
+    test("responds with a 404 status and an arror message ehen given an endpoint that does nor exist", () => {
+      return request(app)
+      .get("/api/not-an-endpoint")
+      .expect(404)
+      .then(({body}) => {
+        expect(body.message).toBe('Not Found')
+      })
+    })
+  })
+
+  describe('GET /api/topics', () => {
+    test('responds with status 200 and an array containing data of all topics', () => {
+      return request(app)
+        .get("/api/topics")
+        .expect(200)
+        .then(({ body }) => {
+          expect(Array.isArray(body.topics)).toBe(true);
+          expect(body.topics.length).toBeGreaterThan(0);
+  
+          expect(body.topics[0]).toEqual({
+            description: 'The man, the Mitch, the legend',
+            slug: 'mitch'
+          });
+  
+          body.topics.forEach(topic => {
+            expect(topic).toEqual(
+              expect.objectContaining({
+                slug: expect.any(String),
+                description: expect.any(String)
+              })
+            );
+          });
+        });
+    });
+  });
+
+  describe('status 500', () => {
+    test("responds with status 500 when database connection fails", () => {
+      jest.spyOn(db, 'query').mockImplementation(() => {
+        throw new Error('Database connection error');
+      });
+  
+      return request(app)
+        .get("/api/topics")
+        .expect(500)
+        .then(response => {
+          expect(response.body.msg).toBe("Internal Server Error");
+        });
+    });
+  })
+
+ 
--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const app = express(); 
 const endpoints = require('./endpoints.json')
+const { getTopics } = require('./controllers/topicsControllers')
 
 app.use(express.json())
 
 app.get('/api', (request, response, next) => {
     response.status(200).send(({endpoints}))
 })
+
+app.get("/api/topics", getTopics)
+
+app.use((req, res, next) => {
+    res.status(404).send({ message: 'Not Found' });
+  });
 
 app.use((error, request, response, next) => {
     if(error.status){
